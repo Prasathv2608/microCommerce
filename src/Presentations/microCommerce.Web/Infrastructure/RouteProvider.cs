c58759f2fccf184721cc953a018a2d3ea725e2ec
Sefa Can <fsefacan@gmail.com>
2018-06-12 23:22:24 +0300

    changed aspnet environment

--- src/Presentations/microCommerce.Web/Infrastructure/RouteProvider.cs
+++ src/Presentations/microCommerce.Web/Infrastructure/RouteProvider.cs
@@ -1,7 +1,6 @@
 ﻿using microCommerce.Mvc.Infrastructure;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Builder;
-using microCommerce.Mvc.Routing;
+using Microsoft.AspNetCore.Routing;
 
 namespace microCommerce.Web.Infrastructure
 {
@@ -18,6 +17,16 @@ namespace microCommerce.Web.Infrastructure
                 name: "Register",
                 template: "Customer/Register",
                 defaults: new { controller = "Customer", action = "Register" });
+
+            routeBuilder.MapRoute(
+                name: "Product",
+                template: "Product",
+                defaults: new { controller = "Product", action = "Index" });
+
+            routeBuilder.MapRoute(
+                name: "ProductDetail",
+                template: "Product/Detail/{Id:int}",
+                defaults: new { controller = "Product", action = "Detail" });
 
             routeBuilder.MapRoute(
                 name: "HomePage",
